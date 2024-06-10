cf3854eb7bb951ec43d6807e09256f5b03aa5cd9
prateek <w.prateek469@gmail.com>
2024-06-10 20:07:36 +0530

    Test

---
+++ src/main/java/com/qa/opencart/pages/Sendmail.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class Sendmail {
+	
+	public void sendeMail() {
+		System.out.println("Mail sent by Pktw");
+	}
+	public void getMail() {
+		System.out.println("Get mail");
+	}
+}
