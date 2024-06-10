280da8b2698394c96a334886359c8262b9bfb55b
prateek <108221268+MoriartysCode@users.noreply.github.com>
2024-06-10 21:43:37 +0530

    Update Mymail.java
    Test

--- src/main/java/com/qa/opencart/pages/Mymail.java
+++ src/main/java/com/qa/opencart/pages/Mymail.java
@@ -2,12 +2,6 @@ package com.qa.opencart.pages;
 
 public class Mymail {
 	public void sent() {
-<<<<<<< Updated upstream
-		System.out.println("Sent by Ratish");
-	}
-
-}
-=======
 		System.out.println("Sent by Bunty");
 	}
 	
