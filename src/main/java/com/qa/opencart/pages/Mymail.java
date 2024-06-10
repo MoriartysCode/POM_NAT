42e111d1135ab478cb8c99de3a4617fc7b8420ce
prateek <w.prateek469@gmail.com>
2024-06-10 21:41:27 +0530

    mail

--- src/main/java/com/qa/opencart/pages/Mymail.java
+++ src/main/java/com/qa/opencart/pages/Mymail.java
@@ -2,7 +2,21 @@ package com.qa.opencart.pages;
 
 public class Mymail {
 	public void sent() {
+<<<<<<< Updated upstream
 		System.out.println("Sent by Ratish");
 	}
 
 }
+=======
+		System.out.println("Sent by Bunty");
+	}
+	
+	public void getMail() {
+		System.out.println("get by Bunty");
+	}
+	
+	
+	
+
+}
+>>>>>>> Stashed changes
