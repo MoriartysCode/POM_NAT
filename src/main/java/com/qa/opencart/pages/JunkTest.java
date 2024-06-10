19eaabcfe4c9a3f9a19940bbf15b4d341866c325
prateek <w.prateek469@gmail.com>
2024-06-10 22:01:18 +0530

    T

---
+++ src/main/java/com/qa/opencart/pages/JunkTest.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class JunkTest {
+	public void sent() {
+		System.out.println("Sent by ratish");
+	}
+	
+	public void get() {
+		System.out.println("get by ratish");
+	}
+
+}
