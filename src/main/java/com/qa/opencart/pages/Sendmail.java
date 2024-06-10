0a64878fb02b44b548c317714281a7056c5dc1ba
prateek <108221268+MoriartysCode@users.noreply.github.com>
2024-06-10 21:01:11 +0530

    Update Sendmail.java
    Commited by ratish

--- src/main/java/com/qa/opencart/pages/Sendmail.java
+++ src/main/java/com/qa/opencart/pages/Sendmail.java
@@ -3,7 +3,7 @@ package com.qa.opencart.pages;
 public class Sendmail {
 	
 	public void sendeMail() {
-		System.out.println("Mail sent by Pktw");
+		System.out.println("Mail sent by Ratish");
 	}
 	public void getMail() {
 		System.out.println("Get mail");
