9b1381858d744caa841987d6bb639a265b0f331f
prateek <w.prateek469@gmail.com>
2024-06-11 16:52:24 +0530

    Added chrome Options

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -22,36 +22,39 @@ public class DriverFactory {
 	public WebDriver driver;
 	public Properties prop;
 	public OptionsManager optionsManager;
-	public static ThreadLocal<WebDriver>tlDriver = new ThreadLocal<WebDriver>();
-	
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
+
 	/**
 	 * this method is initializing the driver on the basis of given Browser name
+	 * 
 	 * @param browserName
 	 * @return- this method return the driver
 	 */
 	public WebDriver initDriver(Properties prop) {
 		optionsManager = new OptionsManager(prop);
-		highlight=prop.getProperty("highlight");
-		
+		highlight = prop.getProperty("highlight");
+
 		String browserName = prop.getProperty("browser").trim().toLowerCase();
-		
-		if(browserName.equalsIgnoreCase("chrome")) {
-			//System.setProperty("webdriver.edge.driver", "C:\\Projects\\JavaBasics\\PktPOM\\Jars\\msedgedriver.exe");
-			//driver = new ChromeDriver(optionsManager.getChromeOprions());
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			// System.setProperty("webdriver.edge.driver",
+			// "C:\\Projects\\JavaBasics\\PktPOM\\Jars\\msedgedriver.exe");
+			// driver = new ChromeDriver(optionsManager.getChromeOprions());
 			tlDriver.set(new ChromeDriver(optionsManager.getChromeOprions()));
-		}else if(browserName.equalsIgnoreCase("firefox")) {
-			//System.setProperty("webdriver.edge.driver", "C:\\Projects\\JavaBasics\\PktPOM\\Jars\\geckodriver.exe");
-			//driver = new FirefoxDriver(optionsManager.getFirefoxOprions());
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			// System.setProperty("webdriver.edge.driver",
+			// "C:\\Projects\\JavaBasics\\PktPOM\\Jars\\geckodriver.exe");
+			// driver = new FirefoxDriver(optionsManager.getFirefoxOprions());
 			tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOprions()));
-		}else if(browserName.equalsIgnoreCase("edge")) {
+		} else if (browserName.equalsIgnoreCase("edge")) {
 			System.setProperty("webdriver.edge.driver", "C:\\Projects\\JavaBasics\\PktPOM\\Jars\\msedgedriver.exe");
-			//driver = new EdgeDriver(optionsManager.getEdgeOprions());
+			// driver = new EdgeDriver(optionsManager.getEdgeOprions());
 			tlDriver.set(new EdgeDriver(optionsManager.getEdgeOprions()));
-		}else {
+		} else {
 			System.out.println("Please pass the correct browser");
 			throw new FrameWorkException("Wrong Browser");
 		}
-		
+
 		getDriver().manage().deleteAllCookies();
 		getDriver().manage().window().maximize();
 		getDriver().get(prop.getProperty("url"));
@@ -60,44 +63,46 @@ public class DriverFactory {
 	/*
 	 * Get the local thread copy of the driver
 	 */
-	
+
 	public synchronized static WebDriver getDriver() {
 		return tlDriver.get();
 	}
+
 	/**
 	 * This method is reading the properties
+	 * 
 	 * @return
 	 */
 	public Properties initProp() {
 		prop = new Properties();
 		FileInputStream ip = null;
-		
-		//mvn clean install -Denv="QA"
+
+		// mvn clean install -Denv="QA"
 		String envName = System.getProperty("env");
 		try {
-			System.out.println("This is the name of environment passed------"+envName.toLowerCase().trim());
-			if(envName==null) {
-				//By default run on QA environment
+			System.out.println("This is the name of environment passed------" + envName.toLowerCase().trim());
+			if (envName == null) {
+				// By default run on QA environment
 				System.out.println("No environment is passed");
 				ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
-			}else
-				switch(envName.toLowerCase().trim()) {
+			} else
+				switch (envName.toLowerCase().trim()) {
 				case "qa":
 					ip = new FileInputStream("./src/test/resources/config/qa.config.properties");
 					break;
 				case "dev":
 					ip = new FileInputStream("./src/test/resources/config/dev.config.properties");
 					break;
-				/*
-				 * default:
-				 * System.out.println("Wrong environment passed No need to run the testCases");
-				 * throw new FrameWorkException("Wrong Environment");
-				 */
 				
-					
+				 default:
+				      System.out.println("Wrong environment passed No need to run the testCases");
+				      throw new FrameWorkException("Wrong Environment");
+				  
+				 
+
 				}
-		}catch(FileNotFoundException e) {
-			
+		} catch (FileNotFoundException e) {
+
 		}
 		try {
 			prop.load(ip);
@@ -106,36 +111,33 @@ public class DriverFactory {
 			e.printStackTrace();
 		}
 		return prop;
-		
-	/*	prop = new Properties();
-		try {
-			FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
-			//FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"src\\test\\resources\\config\\config.properties");
-			prop.load(ip);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return prop;*/
+
+		/*
+		 * prop = new Properties(); try { FileInputStream ip = new
+		 * FileInputStream("./src/test/resources/config/config.properties");
+		 * //FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+
+		 * "src\\test\\resources\\config\\config.properties"); prop.load(ip); } catch
+		 * (FileNotFoundException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }catch (IOException e) { // TODO Auto-generated catch
+		 * block e.printStackTrace(); } return prop;
+		 */
 	}
+
 	/***
 	 * Take ScreenShot
 	 */
 	public static String getScreenshot() {
-		 File srcFile=((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-		 String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
-			File destination = new File(path);
-			try {
-				FileUtil.copyFile(srcFile, destination);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			return path;
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+		File destination = new File(path);
+		try {
+			FileUtil.copyFile(srcFile, destination);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return path;
 	}
 
 }
